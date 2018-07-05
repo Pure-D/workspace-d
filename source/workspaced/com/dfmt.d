071de32eab92f553ea7ffce2c60131e2cc28d4e8
WebFreak001 <gh@webfreak.org>
2018-07-05 19:04:05 +0200

    Fix enforce deprecation warning

--- source/workspaced/com/dfmt.d
+++ source/workspaced/com/dfmt.d
@@ -69,9 +69,9 @@ class DfmtComponent : ComponentWrapper
 					void handleBooleans(string option, string value)
 					{
 						import dfmt.editorconfig : OptionalBoolean;
-						import std.exception : enforceEx;
+						import std.exception : enforce;
 
-						enforceEx!GetOptException(value == "true" || value == "false", "Invalid argument");
+						enforce!GetOptException(value == "true" || value == "false", "Invalid argument");
 						immutable OptionalBoolean val = value == "true" ? OptionalBoolean.t : OptionalBoolean.f;
 						switch (option)
 						{
