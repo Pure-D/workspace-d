8cfb4a99428117289539505d4deb9219460a7681
Jan Jurzitza <gh@webfreak.org>
2020-04-29 10:26:28 +0200

    Upgrade DCD to 0.12.0

--- source/workspaced/com/dcd.d
+++ source/workspaced/com/dcd.d
@@ -32,7 +32,7 @@ class DCDComponent : ComponentWrapper
 {
 	mixin DefaultComponentWrapper;
 
-	enum latestKnownVersion = [0, 11, 1];
+	enum latestKnownVersion = [0, 12, 0];
 	void load()
 	{
 		installedVersion = workspaced.globalConfiguration.get("dcd", "_installedVersion", "");
