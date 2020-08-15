44a86d2b58e9394cb3e468ad88bd2e54a8617046
WebFreak001 <gh@webfreak.org>
2020-08-15 20:22:22 +0200

    bump DCD to 0.13.1

--- source/workspaced/com/dcd.d
+++ source/workspaced/com/dcd.d
@@ -29,7 +29,7 @@ class DCDComponent : ComponentWrapper
 {
 	mixin DefaultComponentWrapper;
 
-	enum latestKnownVersion = [0, 13, 0];
+	enum latestKnownVersion = [0, 13, 1];
 	void load()
 	{
 		installedVersion = workspaced.globalConfiguration.get("dcd", "_installedVersion", "");
